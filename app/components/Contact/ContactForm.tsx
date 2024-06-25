a9a12f9755079bdc3b14a4b88f49e9e82b0136b8
Michał Romak <mrblablak@gmail.com>
2024-06-25 14:24:56 +0200

    Feat: Add error and success message components for ContactForm - Included a close button for the messages to allow users to manually close the notification. - Added timeout functionality to automatically hide the messages after 3 seconds.

--- app/components/Contact/ContactForm.tsx
+++ app/components/Contact/ContactForm.tsx
@@ -1,11 +1,16 @@
 'use client'
-import React, { useRef, useState } from 'react';
+import React, {useRef, useState} from 'react';
 import emailjs from '@emailjs/browser';
+import SuccessMessage from './SuccessMessage'
+import ErrorMessage from './ErrorMessage'
 
 type Props = {};
 
 const ContactForm = ({}: Props) => {
     const form = useRef() as React.MutableRefObject<HTMLFormElement>;
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [showErrorMessage, setShowErrorMessage] = useState(false);
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -13,7 +18,7 @@ const ContactForm = ({}: Props) => {
     });
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const { name, value } = e.target;
+        const {name, value} = e.target;
         setFormData({
             ...formData,
             [name]: value
@@ -22,6 +27,7 @@ const ContactForm = ({}: Props) => {
 
     const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        setIsSubmitting(true);
         const serviceID = process.env.NEXT_PUBLIC_YOUR_SERVICE_ID || '';
         const templateID = process.env.NEXT_PUBLIC_YOUR_TEMPLATE_ID || '';
         const publicKey = process.env.NEXT_PUBLIC_YOUR_PUBLIC_KEY || '';
@@ -38,43 +44,61 @@ const ContactForm = ({}: Props) => {
             .then(
                 () => {
                     console.log('SUCCESS!');
+                    setIsSubmitting(false);
+                    setShowSuccessMessage(true);
+                    setTimeout(() => setShowSuccessMessage(false), 3000);
                 },
                 (error) => {
                     console.log('FAILED...', error.text);
+                    setIsSubmitting(false);
+                    setShowErrorMessage(true);
+                    setTimeout(() => setShowErrorMessage(false), 3000);
                 },
             );
     };
 
     return (
-        <form ref={form} className="flex flex-col pt-10 gap-4" onSubmit={sendEmail}>
-            <input
-                type="text"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                className="input input-bordered w-full"
-                placeholder="Your Name"
-                required
-            />
-            <input
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                className="input input-bordered"
-                placeholder="Your Email"
-                required
-            />
-            <textarea
-                name="message"
-                value={formData.message}
-                onChange={handleChange}
-                className="textarea textarea-bordered h-32"
-                placeholder="Your Message"
-                required
-            ></textarea>
-            <button type="submit" className="btn glass text-white w-40">Send Message</button>
-        </form>
+        <>
+            <form ref={form} className="flex flex-col pt-10 gap-4 pb-2" onSubmit={sendEmail}>
+                <input
+                    type="text"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    className="input input-bordered w-full"
+                    placeholder="Your Name"
+                    required
+                />
+                <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    className="input input-bordered"
+                    placeholder="Your Email"
+                    required
+                />
+                <textarea
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
+                    className="textarea textarea-bordered h-32"
+                    placeholder="Your Message"
+                    required
+                ></textarea>
+                <button disabled={isSubmitting} type="submit" className="btn glass text-white w-40">Send
+                    Message {isSubmitting && <span className="loading loading-spinner loading-sm"></span>}</button>
+            </form>
+
+            {showSuccessMessage && (
+                <SuccessMessage onClose={() => setShowSuccessMessage(false)}/>
+            )}
+            {showErrorMessage && (
+                <ErrorMessage onClose={() => setShowErrorMessage(false)}/>
+            )}
+
+        </>
+
     );
 };
 

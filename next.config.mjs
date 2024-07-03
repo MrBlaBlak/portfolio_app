662de4f20f84393363a9598e952b1a0e5022f08b
Michał Romak <mrblablak@gmail.com>
2024-07-03 08:44:27 +0200

    Fix: Fix themes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    daisyui: {
+        themes: [
+            "dark",
+        ]
+    },
+};
 
 export default nextConfig;
